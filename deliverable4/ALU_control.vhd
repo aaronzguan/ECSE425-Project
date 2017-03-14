fa46c6a0ff68257ab0cce556982ddbb10f8fee8c
klee-17 <kristin.lee0517@gmail.com>
2017-03-14 00:20:52 -0400

    Kristin: Create ALU_Control.vhd skeleton

---
+++ deliverable4/ALU_control.vhd
@@ -0,0 +1,23 @@
+--********************************************************************
+-- ECSE 425, Group 6
+-- Kristin Lee (260509976)
+-- Date: March 13, 2017
+
+-- Description: ALU_control.vhd
+
+--********************************************************************
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ALU_control is
+  port(
+      );
+end ALU_control;
+
+architecture behaviour of ALU_control is
+
+  begin
+  
+end behaviour;
