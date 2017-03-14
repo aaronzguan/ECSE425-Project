51dff95e9bb42fae62a9bf04c5bf50e9a859ea3a
guanzhong1996 <zhong.guan@mail.mcgill.ca>
2017-03-14 16:22:36 -0400

    Add reset, processor_enable and insert_stall
    Update PC as long as we don't stall and processor is be enabled

--- deliverable4/ifprocess.vhd
+++ deliverable4/ifprocess.vhd
@@ -23,6 +23,9 @@ ENTITY ifprocess IS
 	);
 	PORT(
 		clock: IN STD_LOGIC;
+		reset: in std_logic := '0';
+		processor_enable: in std_logic := '1';
+		insert_stall: in std_logic := '0';
 		BranchAddr: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
 		Branch_taken: IN STD_LOGIC := '0';
 		m_addr: OUT INTEGER RANGE 0 TO ram_size -1;
@@ -39,9 +42,15 @@ begin
 process (clock)
 begin
 	if(rising_edge(clock)) then
-		pc <= next_pc;
-		pc_plus4 <= std_logic_vector(unsigned(pc) + 1);
-		m_addr <= to_integer(unsigned(pc));
+		--Synchronous reset
+		if (reset = '1') then
+			pc <= (others => '0');
+		--Update PC as long as we don't stall
+		elsif (processor_enable = '1') and (insert_stall = '0') then
+			pc <= next_pc;
+			pc_plus4 <= std_logic_vector(unsigned(pc) + 1);
+			m_addr <= to_integer(unsigned(pc));
+		end if;
 	end if;
 	if(falling_edge(clock)) then
 		if(Branch_taken = '1') then
