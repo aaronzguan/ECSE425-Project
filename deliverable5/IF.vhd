3265a9cfa29e92a1246d96dfa85313402f7927ea
guanzhong1996 <zhong.guan@mail.mcgill.ca>
2017-04-05 19:13:28 -0400

    Aaron: Update IF

--- deliverable5/IF.vhd
+++ deliverable5/IF.vhd
@@ -14,7 +14,7 @@ entity ifstage IS
 		Branch_taken: in STD_LOGIC := '0';
 		next_addr: out STD_LOGIC_VECTOR (31 DOWNTO 0);
 		
-		cachestartworking: out std_logic := '0'; -- inform cache to start work
+		--cachestartworking: out std_logic := '0'; -- inform cache to start work
 		s_addr_inst: out std_logic_vector(31 downto 0); -- send address to cache
 		s_read_inst: out std_logic; -- send read signal to cache
 		inst: out std_logic_vector(31 downto 0); --  send instruction to ID
@@ -28,7 +28,7 @@ ARCHITECTURE behavioral of ifstage IS
 
 	signal pc: STD_LOGIC_VECTOR (31 DOWNTO 0):= (others => '0');
 	signal pc_plus4: STD_LOGIC_VECTOR (31 DOWNTO 0):= (others => '0');
-	signal cachework: std_logic := '0';
+	signal cachework: std_logic := '0';-- indicating wether the cache is working or not.
 
 begin
 
@@ -48,26 +48,25 @@ begin
 		if (reset = '1') then
 			pc <= (others => '0');
 		end if;
-	end if;
 
-	if(falling_edge(clock)) then
 		if(insert_stall = '0') then
-			if (cachework = '0') then
+			if(cachework = '0') then -- send the pc if the cache is not working
 				pc_plus4 <= std_logic_vector(to_unsigned( to_integer(unsigned(pc)) + 4,32));
 				next_addr <= pc;
 				s_addr_inst <= pc; -- send the next pc
 				s_read_inst <= '1'; -- send the read signal to cache
-				cachestartworking <= '1'; -- let cache work so that s_wairt_request is activated
 				cachework <= '1';
-			elsif (falling_edge(s_waitrequest_inst)) then
-				pc_plus4 <= std_logic_vector(to_unsigned( to_integer(unsigned(pc)) + 4,32));
-				next_addr <= pc;
-				s_addr_inst <= pc; -- send the next pc 
-				s_read_inst <= '1'; -- send the read signal to cache and let cache work
+			end if;
+		end if;
+
+	elsif(falling_edge(clock)) then
+		if(insert_stall = '0') then
+			if (s_waitrequest_inst = '0') then -- IF can receive the results
 				inst <= s_readdata_inst; -- get instruction from cache
-		
+				s_read_inst <= '0';
+				cachework <= '0';
 			elsif (s_waitrequest_inst = '1') then
-				inst <= x"00000020"; -- If read miss
+				inst <= x"00000020"; -- If read miss, send 0+0=0 to ID
 			end if;
 		end if;
 	end if;
