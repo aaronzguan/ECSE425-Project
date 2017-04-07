c5b9277eda3e068659343fe107614676c10d3877
zhangrunze199511 <runze.zhang@mail.mcgill.ca>
2017-04-07 00:14:24 -0400

    modified, compiled

--- deliverable5/Cache.vhd
+++ deliverable5/Cache.vhd
@@ -20,9 +20,10 @@ port(
     
 	m_addr : out integer range 0 to ram_size-1;
 	m_read : out std_logic;
-	m_readdata : in std_logic_vector (7 downto 0);
+	m_readdata : in std_logic_vector (31 downto 0);
 	m_write : out std_logic;
-	m_writedata : out std_logic_vector (7 downto 0);
+	m_writedata : out std_logic_vector (31 downto 0);
+        ismiss: out std_logic;
 	m_waitrequest : in std_logic
 
 	--cachework : in std_logic := '0'
@@ -45,7 +46,7 @@ architecture arch of InstCache is
 	signal valid: std_logic := '0';
 	signal dirty: std_logic := '0';
 	signal offset: integer range 0 to 3; 
-	signal ref_counter1 : integer := 0;
+	signal ref_counter1 : integer := 1;
         signal ref_counter2 : integer := 0;
 	--signal b_counter : integer := 0;
 	--signal s_write_waitreq_reg: std_logic := '1';
@@ -61,8 +62,8 @@ architecture arch of InstCache is
         signal invoke_writeback: std_logic;
         signal invoke_memread: std_logic;
         signal m_writedata_temp: std_logic_vector(31 downto 0);
-        signal m_readdata_temp: std_logic_vector(31 downto 0); 
-        signal m_addr_temp : std_logic_vector(31 downto 0);
+        --signal m_readdata_temp: std_logic_vector(31 downto 0); 
+        signal m_addr_temp : integer;
         signal m_read_temp : std_logic;
         signal m_write_temp : std_logic;
         signal wb_stage: std_logic;
@@ -70,11 +71,21 @@ architecture arch of InstCache is
         signal wb_finish: std_logic;
         signal mr_finish : std_logic;
         signal mem_finish: std_logic;
+        signal wb_start: std_logic;
+        signal mr_start: std_logic;   
  
 begin
 
+        s_readdata <= s_readdata_temp;
+        s_waitrequest <= s_waitrequest_temp;
+        m_writedata<= m_writedata_temp;
+        m_addr <= m_addr_temp;
+        m_read<= m_read_temp;
+        m_write<= m_write_temp;
+       
+
      	addr_word_offset <= s_addr(3 downto 2); -- word offset of address
-	addr_byte_offset <= s_addr (1 downto 0); -- byte offset 
+	--addr_byte_offset <= s_addr (1 downto 0); -- byte offset 
 	addr_index <= s_addr(8 downto 4); -- index of address
 	addr_tag <= s_addr(14 downto 9); -- tag of address
 	index <= to_integer(unsigned(addr_index)); --index
@@ -86,12 +97,16 @@ begin
 
 idleNcomp: process(s_read,mem_finish)
 begin 
+     if(rising_edge(mem_finish))then 
+        ismiss<= '0';
+       end if;
     if(rising_edge(s_read) or rising_edge(mem_finish))then 
        if(addr_tag = block_tag and valid = '1')then 
             --hit <= '1';
                s_readdata_temp<= cache(index)(32*(to_integer(unsigned(addr_word_offset)))+31 downto 32*(to_integer(unsigned(addr_word_offset))));
                s_waitrequest_temp <= '0';        
         else  
+                ismiss<= '1';
                if(dirty = '1') then 
                    invoke_writeback<= '1';
                 else 
@@ -109,37 +124,32 @@ end process;
 memAccess: process(invoke_writeback, invoke_memread,wb_finish,mr_finish)
 begin
     if(rising_edge(invoke_writeback))then 
-       --  m_writedata_temp <= ;
-         m_write_temp<= '1';
-        -- m_addr_temp <= ;
-         wb_stage<= '1'; 
+        wb_start <= '1';
      elsif(rising_edge(invoke_memread))then 
-        -- m_readdata_temp <= ;
-         m_read_temp<='1';
-        -- m_addr_temp <= ;
-         mr_stage<= '1';
+        mr_start <= '1';
      elsif(falling_edge(invoke_writeback))then 
-         m_write_temp <='0';
+        wb_start <= '0';
+     --  m_write_temp <='0';
      elsif(falling_edge(invoke_memread)) then 
-         m_read_temp<='0';
+        mr_start <= '0';    
+     -- m_read_temp<='0';
       end if;
       if(rising_edge(wb_finish)) then 
          wb_stage <= '0';
-          -- m_readdata_temp <= ;
-         m_read_temp<='1';
-        -- m_addr_temp <= ;
-         mr_stage<= '1';
+         mr_start<= '1';
         elsif(rising_edge(mr_finish)) then
          mr_stage<= '0'; 
          mem_finish<= '1';
          elsif(falling_edge(wb_finish))then 
-             m_read_temp<= '0';
+            mr_start<= '0';
          elsif(falling_edge(mr_finish))then 
              mem_finish <= '0';       
        end if;
+
+
 end process;
 
-WBstage: process(m_waitrequest)
+WBnMRstage: process(m_waitrequest,wb_start,mr_start)
 begin 
      if(wb_stage = '1' and rising_edge(m_waitrequest)) then 
         if(ref_counter1 = 4)then 
@@ -147,33 +157,48 @@ begin
           ref_counter1 <= 1;
           else 
           m_write_temp <= '1';
-          --m_writedata_temp<= ;
-         -- m_addr_temp <= ;
+          m_writedata_temp<= cache (index) ( ref_counter1*32+31 downto ref_counter1*32);
+          m_addr_temp <=((to_integer(unsigned(block_tag))*512)+(to_integer(unsigned(addr_index))*16)+ref_counter1*4);
           ref_counter1 <= ref_counter1+1;
         end if;
      elsif(wb_stage = '1' and falling_edge(m_waitrequest)) then 
            m_write_temp <= '0';
            wb_finish <= '0';
       end if;
-end process;
 
-MRstage: process(m_waitrequest)
-begin  
-     if(mr_stage = '1' and rising_edge(m_waitrequest)) then 
-         --m_readdata_temp<=  ;
-        if(ref_counter2 = 4)then 
+if(mr_stage = '1' and rising_edge(m_waitrequest)) then 
+         cache(index)(ref_counter2*32+31 downto ref_counter2*32)<= m_readdata;
+        if(ref_counter2 = 3)then 
           mr_finish <= '1';
-          ref_counter2 <= 1;
-          else 
+          ref_counter2 <= 0;
+         else 
           m_read_temp <= '1';
-  --        m_addr_temp <= ;
+         m_addr_temp <=((to_integer(unsigned(addr_tag))*512)+(to_integer(unsigned(addr_index))*16)+(ref_counter2+1)*4);
           ref_counter2 <= ref_counter2+1;
         end if;
      elsif(mr_stage = '1' and falling_edge(m_waitrequest)) then 
            m_read_temp <= '0';
            mr_finish <= '0';
       end if;
+
+if(rising_edge(wb_start))then 
+         m_writedata_temp <= cache (index) (31 downto 0);
+         m_write_temp<= '1';
+         m_addr_temp <=((to_integer(unsigned(block_tag))*512)+(to_integer(unsigned(addr_index))*16));
+         wb_stage<= '1'; 
+elsif(rising_edge(mr_start)) then 
+         m_read_temp<='1';
+         m_addr_temp <=((to_integer(unsigned(addr_tag))*512)+(to_integer(unsigned(addr_index))*16));
+         mr_stage<= '1';
+elsif(falling_edge(wb_start))then 
+         m_write_temp <='0';
+elsif(falling_edge(mr_start))then 
+        m_read_temp<='0';
+
+end if;
+
 end process;
+
 
 
 
