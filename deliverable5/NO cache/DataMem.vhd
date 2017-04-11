9d72fcdebd3b9d60bc8928d71103601e270f1c5d
zhangrunze199511 <runze.zhang@mail.mcgill.ca>
2017-04-11 13:55:28 -0400

    Update DataMem.vhd

--- deliverable5/NO cache/DataMem.vhd
+++ deliverable5/NO cache/DataMem.vhd
@@ -31,14 +31,14 @@ entity DataMem is
 	    
 
                  mem_data_stall_in: in std_logic;
-                 mem_data_stall: out std_logic;
+                 mem_data_stall: out std_logic:='0';
 
 		--cachestartworkting: out std_logic := '0'; -- inform data cache start to work
-		s_addr_data:out integer; -- send address to cache
-		s_read_data: out std_logic; -- send read signal to cache
+		s_addr_data:out integer:= 0; -- send address to cache
+		s_read_data: out std_logic:='0'; -- send read signal to cache
 		s_readdata_data: in std_logic_vector(31 downto 0); -- get data from cache
-		s_write_data: out std_logic; -- send write signal to cache
-		s_writedata_data: out std_logic_vector(31 downto 0);-- send the writedata to cache
+		s_write_data: out std_logic:='0'; -- send write signal to cache
+		s_writedata_data: out std_logic_vector(31 downto 0):=(others => '0');-- send the writedata to cache
 		s_waitrequest_data: in std_logic := '0' --get waitrequest signal from cache
                  
 			
@@ -51,14 +51,20 @@ architecture behavior of DataMem is
 	signal reading: std_logic := '0';
 begin
  MEM_control_buffer_out<= MEM_control_buffer;
-
-
-     process(clock)
+ 	 bran_addr <= bran_addr_in;
+ 
+      extra:process(ALU_result)
+      begin
+        if(mem_data_stall_in  = '0')then 
+        s_addr_data <= to_integer(unsigned( ALU_result));
+         end if;
+      end process;
+     process(clock,s_waitrequest_data)
      begin
 	
        if(rising_edge(clock))then
 		dest_addr_out <= dest_addr_in;
-          	bran_addr <= bran_addr_in;
+          
           	bran_taken_out<= bran_taken;
 		
         
@@ -66,11 +72,10 @@ begin
         		--if(opcode = "000101" or opcode = "000100")then
         
         	------ the opcode is sw -----
-        	if(opcode = "101011")then
-                        mem_data_stall <= '1';         		
+        	if(opcode = "101011")then                  		
    -- bran_addr <= std_logic_vector(to_unsigned(0, 32));
 			if(cachework = '0') then
-				s_addr_data <= to_integer(unsigned(ALU_result));
+				
 				s_writedata_data <= rt_data;
 				s_write_data <= '1';
          			cachework <= '1';
@@ -78,11 +83,9 @@ begin
 			end if;
 			
        		------ the opcode is lw -----
-        	elsif(opcode = "100011")then
-                       mem_data_stall <= '1';         	
+        	elsif(opcode = "100011")then     
 			if(cachework = '0') then
        			--  bran_addr <= std_logic_vector(to_unsigned(0, 32));
-				s_addr_data <= to_integer(unsigned(ALU_result));
 				s_read_data <= '1';
 				cachework <= '1';
 				reading <= '1';
@@ -95,20 +98,31 @@ begin
 			
 	elsif(falling_edge(clock))then
 		WB_control_buffer_out<= WB_control_buffer;
-		if(s_waitrequest_data = '0' and writing = '1') then
+		s_write_data <= '0';
+                s_read_data <= '0';
+                if((opcode = "101011" or opcode = "100011") and  s_waitrequest_data = '1')  then
+                        mem_data_stall <= '1';       
+                 end if;
+
+	end if;
+	 				
+       
+        if(falling_edge(s_waitrequest_data))then 
+
+              if( writing = '1') then
                                 mem_data_stall <= '0';         
-				s_write_data <= '0';
 				writing <= '0';
 				cachework <= '0';
-		elsif(s_waitrequest_data = '0' and reading = '1') then
+	      elsif(reading = '1') then
                                 mem_data_stall <= '0';     
 				mem_data <= s_readdata_data;
-				s_read_data <= '0';
 				cachework <= '0';
 				reading <= '0';
 		end if;
-				
-       	end if;
+         end if;
+     
+
+
     end process;
 	  	
 end behavior;
