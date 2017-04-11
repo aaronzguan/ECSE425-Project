c2b725be8ae70c646c1c64e40194e04281ae3631
zhangrunze199511 <runze.zhang@mail.mcgill.ca>
2017-04-11 13:54:39 -0400

    Update IF.vhd

--- deliverable5/NO cache/IF.vhd
+++ deliverable5/NO cache/IF.vhd
@@ -5,19 +5,19 @@ USE ieee.numeric_std.all;
 entity ifstage IS
 	PORT(
 		clock: in STD_LOGIC;
-		reset: in std_logic;
-		insert_stall: in std_logic;
+		reset: in std_logic := '0';
+		insert_stall: in std_logic := '0';
 		BranchAddr: in STD_LOGIC_VECTOR (31 DOWNTO 0);
-		Branch_taken: in STD_LOGIC;
+		Branch_taken: in STD_LOGIC := '0';
 		next_addr: out STD_LOGIC_VECTOR (31 DOWNTO 0);
-		
-		s_addr_inst: out std_logic_vector(31 downto 0); -- send address to cache
+		max_inst: in integer;
+		s_addr_inst: out integer:=0; -- send address to cache
 		s_read_inst: out std_logic; -- send read signal to cache
 		inst: out std_logic_vector(31 downto 0); --  send instruction to ID
-		s_waitrequest_inst: in std_logic; -- get waitrequest signal from cache
+		s_waitrequest_inst: in std_logic :='0'; -- get waitrequest signal from cache
 		s_readdata_inst: in std_logic_vector(31 downto 0); -- get instruction from cache
                 mem_data_stall: in std_logic; 
-		ismiss: in std_logic
+		ismiss: in std_logic := '0'
 		
 	);
 END ifstage;
@@ -26,51 +26,68 @@ ARCHITECTURE behavioral of ifstage IS
 
 	signal pc: STD_LOGIC_VECTOR (31 DOWNTO 0):= (others => '0');
 	signal pc_plus4: STD_LOGIC_VECTOR (31 DOWNTO 0):= (others => '0');
-	signal cachework: std_logic := '0';-- indicating wether the cache is working or not.
-
+    signal one_delay:std_logic:='0';
+    signal no_accept:std_logic:='0';
 begin
 
 process (pc_plus4,Branch_taken)
 begin
-	if((Branch_taken = '1') and (insert_stall = '0') and (mem_data_stall = '0'))then
+	if(rising_edge(Branch_taken)) and (insert_stall = '0') and (mem_data_stall = '0')then
 		pc <= BranchAddr;
-	elsif  (insert_stall = '0' and mem_data_stall = '0') then
+	elsif  (insert_stall = '0' and mem_data_stall = '0' and pc_plus4'event ) then
 		pc <= pc_plus4;               
 	end if;
 end process;
 
-
-process(ismiss,s_waitrequest_inst,clock)
+process(ismiss,s_waitrequest_inst,clock,Branch_taken,mem_data_stall)
 begin
-	if(rising_edge(clock)) then
-		--Synchronous reset
-		if (reset = '1') then
-			pc <= (others => '0');
-		end if;
-		s_read_inst <= '0';
-	end if;
-
 	if(insert_stall = '0' and mem_data_stall = '0') then
+               if(rising_edge(Branch_taken)) then
+                  s_addr_inst <= to_integer(unsigned( BranchAddr)); 
+
+                 end if;
+	       if(rising_edge(clock)) then
+	            s_read_inst <= '0';
+                   if(ismiss = '0') then 
+                     s_addr_inst <=  to_integer(unsigned(pc)); -- send the address to cache   
+                    end if;
+                end if;
+		--if(rising_edge(ismiss)) then
+		--	inst <= x"00000020"; -- read miss, send 0+0=0 to ID
+		if(falling_edge(clock) and ismiss = '1') then
+			inst <= x"00000020"; -- read miss, send 0+0=0 to ID
+		elsif(falling_edge(clock) and ismiss = '0' and one_delay = '0') then
+			one_delay<= '1';
+         elsif(falling_edge(clock) and one_delay = '1') then 
+            inst <= x"00000020"; -- read miss, send 0+0=0 to ID
+            pc_plus4 <= std_logic_vector(to_unsigned( to_integer(unsigned(pc)) + 4,32));
+			next_addr <= pc;
+			--if( to_integer(unsigned(pc)) < max_inst*4) then 
+            s_read_inst <= '1'; -- send the read signal to cache
+            one_delay <= '0';
+			--end if;
+		end if;
+		end if;
+        
+        if(rising_edge(Branch_taken))then 
+          no_accept <= '1';
+       end if;
+        if(falling_edge(mem_data_stall))then 
+             one_delay <= '1';
+          end if;
+        
+        if (falling_edge(s_waitrequest_inst)) then -- IF can receive the results
+	             report "swr invoke this";
+                 if( to_integer(unsigned(pc)) > max_inst*4 and one_delay ='0') then         
+                        
+                        inst <= x"00000020";  
+                     elsif(no_accept ='1')then 
+                           inst <= x"00000020";  
+                           no_accept<= '0';
+                         else
+                         inst <= s_readdata_inst; -- get instruction from cache
+		          end if;	
+		end if;
 	
-		if(rising_edge(ismiss)) then
-			inst <= x"00000020"; -- read miss, send 0+0=0 to ID
-		elsif(falling_edge(clock) and ismiss = '1') then
-			inst <= x"00000020"; -- read miss, send 0+0=0 to ID
-		elsif(falling_edge(clock) and ismiss = '0') then  -- ismiss = U
-			if(cachework='0') then
-				s_read_inst <= '1';
-				pc_plus4 <= std_logic_vector(to_unsigned( to_integer(unsigned(pc)) + 4,32));
-				next_addr <= pc;
-				s_addr_inst <= pc; -- send the address to cache
-				cachework <= '1';
-			end if;
-		end if;
-		if (falling_edge(s_waitrequest_inst)) then -- IF can receive the results
-			inst <= s_readdata_inst; -- get instruction from cache
-			if(cachework = '1') then
-				cachework <= '0';
-			end if;
-		end if;
-	end if;
 end process;
 end behavioral;
